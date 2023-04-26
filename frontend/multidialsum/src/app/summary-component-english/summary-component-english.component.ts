7d5dc865c1469874b71ea2dc405adf6031dc0fd2
mr-desilva <andrewdesilvasl@gmail.com>
2023-04-26 14:29:34 +0530

    added mock responses

--- frontend/multidialsum/src/app/summary-component-english/summary-component-english.component.ts
+++ frontend/multidialsum/src/app/summary-component-english/summary-component-english.component.ts
@@ -16,10 +16,24 @@ export class SummaryComponentEnglishComponent implements OnInit {
   summary: string = '';
   loading: boolean = false;
   summaryGenerated: boolean = false;
+  
+  selectedOption: string | undefined;
 
   constructor(private http: HttpClient, private db: AngularFireDatabase) {}
 
   ngOnInit(): void {}
+  //const customerMessage1 = "Hi, I'm having trouble logging into my account. Can you help me? Support Agent: Of course, I'd be happy to help. Can you tell me your username and the email address associated with your account? Customer: My username is johnsmith42 and the email is johnsmith42@yahoo.com Support Agent: Thanks, let me take a look at your account. It looks like there's been some suspicious activity on your account, so we've temporarily disabled it for security reasons. Customer: Oh no, what kind of activity? Support Agent: It looks like someone tried to log into your account from a different country, which triggered our security system. But don't worry, we can help you reactivate your account. Can you verify some information for me, such as your full name and date of birth? Customer: Sure, my name is John Smith and my date of birth is April 5th, 1990 Support Agent: Great, thanks for verifying that information. I'm going to send a verification code to your email address. Once you receive it, please enter it on the login screen to reactivate your account. Customer: Okay, I'll keep an eye out for the email. Thank you for your help! Support Agent: You're welcome. Let me know if you have any other questions or concerns";
+
+
+  onOptionSelected() {
+    if (this.selectedOption === 'option1') {
+      this.dialogue = "Customer: Hi, I'm having trouble logging into my account. Can you help me?\n\nSupport Agent: Of course, I'd be happy to help. Can you tell me your username and the email address associated with your account?\n\nCustomer: My username is johnsmith42 and the email is johnsmith42@yahoo.com\nSupport Agent: Thanks, let me take a look at your account. It looks like there's been some suspicious activity on your account, so we've temporarily disabled it for security reasons.\n\nCustomer: Oh no, what kind of activity?\n\nSupport Agent: It looks like someone tried to log into your account from a different country, which triggered our security system. But don't worry, we can help you reactivate your account. Can you verify some information for me, such as your full name and date of birth?\n\nCustomer: Sure, my name is John Smith and my date of birth is April 5th, 1990\n\nSupport Agent: Great, thanks for verifying that information. I'm going to send a verification code to your email address. Once you receive it, please enter it on the login screen to reactivate your account.\n\nCustomer: Okay, I'll keep an eye out for the email. Thank you for your help!\n\nSupport Agent: You're welcome. Let me know if you have any other questions or concerns";
+    } else if (this.selectedOption === 'option2') {
+      this.dialogue = "Customer: Hey, I need to check why my phone is loosing signal ?\n\nSupport: Hello, can I know the phone modal?\n\nCustomer: Yes, this is a iphone 14?\n\nSupport: Did you check for any software updates ? if not please update your device to latest version. \n\nCustomer: Yes, there is a software update, I'll try and let you know.\n\nSupport: Sure\n\nCustomer: I have updates the software to the latest version and now the signal loosing issue is resolved. \n\nSupport: Happy to help.";
+    } else if (this.selectedOption === 'option3') {
+      this.dialogue = "customer: My watchlist is not updating with new episodes (past couple days).  Any idea why?\n\nsupport: Apologies for the trouble, Norlene! We're looking into this. In the meantime, try navigating to the season / episode manually.\n\ncustomer: Tried logging out/back in, that didn’t help\n\nsupport: Sorry! 😔 We assure you that our team is working hard to investigate, and we hope to have a fix ready soon!\n\ncustomer: Thank you! Some shows updated overnight\n\nsupport: Awesome! That's what we love to hear. If you happen to need anything else, we'll be here to support! 💚";
+    }
+  }
 
   onInputChange() {
     if (this.dialogue === '') {
@@ -57,6 +71,35 @@ export class SummaryComponentEnglishComponent implements OnInit {
     
   }
 
+  // generateSummary() {
+  //   if (this.dialogue.trim() === '') {
+  //     Swal.fire({
+  //       icon: 'warning',
+  //       title: 'Alert',
+  //       text: 'Please enter the dialogue before generating a summary.',
+  //     });
+  //     return;
+  //   }
+  //   this.loading = true;
+  //   const apiUrl = 'http://localhost:5000/api/summarize';
+
+  //   // Send the input to the Flask backend
+  //   this.http.post(apiUrl, { text: this.dialogue }).subscribe(
+  //     (response: any) => {
+  //       // Update the summary with the response from the Flask backend
+  //       this.summary = response.summary;
+  //       this.language = this.getLanguageName(response.lang);
+  //       this.loading = false;
+  //       this.summaryGenerated = true;
+  //     },
+  //     (error) => {
+  //       console.error('Error fetching summary:', error);
+  //       this.loading = false;
+  //     }
+  //   );
+  // }
+
+  // Use this for mock development
   generateSummary() {
     if (this.dialogue.trim() === '') {
       Swal.fire({
@@ -67,48 +110,42 @@ export class SummaryComponentEnglishComponent implements OnInit {
       return;
     }
     this.loading = true;
-    const apiUrl = 'http://localhost:5000/api/summarize';
 
-    // Send the input to the Flask backend
-    this.http.post(apiUrl, { text: this.dialogue }).subscribe(
-      (response: any) => {
-        // Update the summary with the response from the Flask backend
-        this.summary = response.summary;
-        this.language = this.getLanguageName(response.lang);
-        this.loading = false;
-        this.summaryGenerated = true;
-      },
-      (error) => {
-        console.error('Error fetching summary:', error);
-        this.loading = false;
-      }
-    );
-  }
+    // Mock JSON response object
+    const mockResponse = {
+      summary: 'This is a mock summary of the english input text.', lang: 'en'
+    };
 
-  // Use this for mock development
-  // generateSummary() {
+    // Mock JSON response object
+    const mockResponse1 = {
+      summary: 'Customer is complaining that they are unable to login into their Yahoo account. Agent states that they had some suspicious activity on their account, so they have temporarily disabled it for security reasons.', lang: 'en'
+    };
 
-  //   if (this.dialogue.trim() === '') {
-  //     Swal.fire({
-  //       icon: 'warning',
-  //       title: 'Alert',
-  //       text: 'Please enter the dialogue before generating a summary.',
-  //     });
-  //     return;
-  //   }
-  //   this.loading = true;
+    const mockResponse2 = {
+      summary: 'Customer is complaining about the phone which is losing signal. Agent updated the customer to update the device to latest version.', lang: 'en'
+    };
 
-  //   // Mock JSON response object
-  //   const mockResponse = {
-  //     summary: 'This is a mock summary of the english input text.', lang: 'en'
-  //   };
+    const mockResponse3 = {
+      summary: 'Customer is complaining that their watchlist is not updating with new episodes. Agent updates that their team is working hard to investigate and they hope to have a fix ready soon.', lang: 'en'
+    };
   
-  //   // Simulate an API call delay using setTimeout
-  //   setTimeout(() => {
-  //     this.summary = mockResponse.summary;
-  //     this.language = this.getLanguageName(mockResponse.lang);
-  //     this.loading = false;
-  //     this.summaryGenerated = true;
-  //   }, 3000); // Adjust the delay (in milliseconds) as needed
-  // }
+    // Simulate an API call delay using setTimeout
+    setTimeout(() => {
+      if(this.selectedOption == "option1") {
+        this.summary = mockResponse1.summary;
+      }
+      else if(this.selectedOption == "option2") {
+        this.summary = mockResponse2.summary;
+      }
+      else if(this.selectedOption == "option3") {
+        this.summary = mockResponse3.summary;
+      }
+      else {
+        this.summary = mockResponse.summary;
+      }
+      this.language = this.getLanguageName(mockResponse.lang);
+      this.loading = false;
+      this.summaryGenerated = true;
+    }, 1000); // Adjust the delay (in milliseconds) as needed
+  }
 }
--- frontend/multidialsum/src/app/summary-component/summary-component.component.ts
+++ frontend/multidialsum/src/app/summary-component/summary-component.component.ts
@@ -16,9 +16,23 @@ export class SummaryComponentComponent implements OnInit {
   loading: boolean = false;
   summaryGenerated: boolean = false;
 
+  selectedOption: string | undefined;
+
   constructor(private http: HttpClient, private db: AngularFireDatabase) {}
 
   ngOnInit(): void {}
+
+
+  onOptionSelected() {
+    if (this.selectedOption === 'option1') {
+      this.dialogue = "客户：您好，我的账户有问题。你能帮助我吗\n\n支持:当然！您能否向我提供有关您遇到的问题的更多信息\n\n客户:我好像无法登录我的帐户。我已经尝试重置我的密码，但我仍然无法进入\n\n支持:能否请您提供您注册的电子邮件地址\n\n客户:是的，是 john.doe@example.com\n\n支持:我将向您的电子邮件地址发送一个密码重置链接。请检查您的收件箱并按照电子邮件中的说明进行操作\n\n客户:好的，我现在看到邮件了。我正在重置我的密码\n\n支持:太好了！请创建一个新的安全密码, 然后再次尝试登录\n\n客户: 成功了！感谢您的帮助\n\n支持: 不客气"
+      ;
+    } else if (this.selectedOption === 'option2') {
+      this.dialogue = "Kunde: Hallo, ich habe ein Problem mit meinem Konto. Kannst du mir helfen?\n\nUnterstützung: Natürlich! Können Sie mir bitte weitere Informationen zu dem Problem geben, das Sie haben?\n\nKunde: Ich kann mich anscheinend nicht bei meinem Konto anmelden. Ich habe versucht, mein Passwort zurückzusetzen, aber ich kann mich immer noch nicht anmelden.\n\nSupport: Können Sie mir bitte Ihre registrierte E-Mail-Adresse mitteilen?\n\nKunde: Ja, es ist john.doe@example.com.\n\nSupport: Ich werde Ihnen einen Link zum Zurücksetzen des Passworts an Ihre E-Mail-Adresse senden. Bitte überprüfen Sie Ihren Posteingang und folgen Sie den Anweisungen in der E-Mail.\n\nKunde: Okay, ich sehe die E-Mail jetzt. Ich setze mein Passwort zurück.\n\nUnterstützung: Super! Bitte erstellen Sie ein neues, sicheres Passwort und versuchen Sie dann erneut, sich anzumelden.\n\nKunde: Es hat funktioniert! Vielen Dank für Ihre Hilfe!";
+    } else if (this.selectedOption === 'option3') {
+      this.dialogue = "Cliente: Hola, tengo algunos problemas con mi cuenta. ¿Me puedes ayudar?\n\nSoporte: ¡Por supuesto! ¿Puede por favor proporcionarme más información sobre el problema que está experimentando?\n\nCliente: Parece que no puedo iniciar sesión en mi cuenta. He intentado restablecer mi contraseña, pero sigo sin poder entrar.\n\nSoporte: ¿Puede proporcionarme su dirección de correo electrónico registrada?\n\nCliente: Sí, es john.doe@example.com.\n\nSoporte: Voy a enviarle un enlace de restablecimiento de contraseña a su dirección de correo electrónico. Por favor revise su bandeja de entrada y siga las instrucciones en el correo electrónico.\n\nCliente: Bien, ahora veo el correo electrónico. Estoy restableciendo mi contraseña.\n\nSoporte: ¡Genial! Cree una nueva contraseña segura y luego intente iniciar sesión nuevamente.\n\nCliente: ¡Funcionó! ¡Gracias por su ayuda!\n\nSoporte: De na";
+    }
+  }
 
   onInputChange() {
     if (this.dialogue === '') {
@@ -56,6 +70,35 @@ export class SummaryComponentComponent implements OnInit {
     
   }
 
+  // generateSummary() {
+  //   if (this.dialogue.trim() === '') {
+  //     Swal.fire({
+  //       icon: 'warning',
+  //       title: 'Alert',
+  //       text: 'Please enter the dialogue before generating a summary.',
+  //     });
+  //     return;
+  //   }
+  //   this.loading = true;
+  //   const apiUrl = 'http://localhost:5000/api/summarize/multi';
+
+  //   // Send the input to the Flask backend
+  //   this.http.post(apiUrl, { text: this.dialogue }).subscribe(
+  //     (response: any) => {
+  //       // Update the summary with the response from the Flask backend
+  //       this.summary = response.summary;
+  //       this.language = this.getLanguageName(response.lang);
+  //       this.loading = false;
+  //       this.summaryGenerated = true;
+  //     },
+  //     (error) => {
+  //       console.error('Error fetching summary:', error);
+  //       this.loading = false;
+  //     }
+  //   );
+  // }
+
+
   generateSummary() {
     if (this.dialogue.trim() === '') {
       Swal.fire({
@@ -66,48 +109,47 @@ export class SummaryComponentComponent implements OnInit {
       return;
     }
     this.loading = true;
-    const apiUrl = 'http://localhost:5000/api/summarize/multi';
 
-    // Send the input to the Flask backend
-    this.http.post(apiUrl, { text: this.dialogue }).subscribe(
-      (response: any) => {
-        // Update the summary with the response from the Flask backend
-        this.summary = response.summary;
-        this.language = this.getLanguageName(response.lang);
-        this.loading = false;
-        this.summaryGenerated = true;
-      },
-      (error) => {
-        console.error('Error fetching summary:', error);
-        this.loading = false;
+    // Mock JSON response object
+    const mockResponse = {
+      summary: 'This is a mock summary of the english input text.', lang: 'en'
+    };
+
+    // Mock JSON response object
+    const mockResponse1 = {
+      summary: '客户抱怨他们无法登录他们的帐户代理更新他们将向他们的电子邮件地址发送密码重置链接。', lang: 'zh'
+    };
+
+    const mockResponse2 = {
+      summary: 'Der Kunde klagt, dass er nicht in der Lage ist, sich in sein Konto anzumelden. Agent sagt, dass er ihnen einen Link senden wird, um das Passwort auf ihre E-Mail-Adresse wiederherzustellen.', lang: 'de'
+    };
+
+    const mockResponse3 = {
+      summary: 'El cliente se queja de que no puede entrar en su cuenta. el agente le propone revisar su cuadro de entrada y seguir las instrucciones en el correo electrónico.', lang: 'es'
+    };
+  
+    // Simulate an API call delay using setTimeout
+    setTimeout(() => {
+      if(this.selectedOption == "option1") {
+        this.summary = mockResponse1.summary;
+        this.language = this.getLanguageName(mockResponse1.lang);
       }
-    );
+      else if(this.selectedOption == "option2") {
+        this.summary = mockResponse2.summary;
+        this.language = this.getLanguageName(mockResponse2.lang);
+      }
+      else if(this.selectedOption == "option3") {
+        this.summary = mockResponse3.summary;
+        this.language = this.getLanguageName(mockResponse3.lang);
+      }
+      else {
+        this.summary = mockResponse.summary;
+        this.language = this.getLanguageName(mockResponse.lang);
+      }
+      this.loading = false;
+      this.summaryGenerated = true;
+    }, 1000); // Adjust the delay (in milliseconds) as needed
   }
 
-  // Use this for mock development
-  // generateSummary() {
-
-  //   if (this.dialogue.trim() === '') {
-  //     Swal.fire({
-  //       icon: 'warning',
-  //       title: 'Alert',
-  //       text: 'Please enter the dialogue before generating a summary.',
-  //     });
-  //     return;
-  //   }
-  //   this.loading = true;
-
-  //   // Mock JSON response object
-  //   const mockResponse = {
-  //     summary: 'This is a mock summary of the input text.', lang: 'en'
-  //   };
   
-  //   // Simulate an API call delay using setTimeout
-  //   setTimeout(() => {
-  //     this.summary = mockResponse.summary;
-  //     this.language = this.getLanguageName(mockResponse.lang);
-  //     this.loading = false;
-  //     this.summaryGenerated = true;
-  //   }, 3000); // Adjust the delay (in milliseconds) as needed
-  // }
 }
